f13bcf632e5b6318f6199c7ead5a1a414f552bd2
karpenkoYegor <77341024+karpenkoYegor@users.noreply.github.com>
2021-10-19 20:47:41 +0300

    Добавьте файлы проекта.

---
+++ LeverXNetLab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeverXNetLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
