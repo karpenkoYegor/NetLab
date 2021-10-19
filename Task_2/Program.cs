cdd0832e592271e7bb317a81b4871d3d7b765cbe
karpenkoYegor <77341024+karpenkoYegor@users.noreply.github.com>
2021-10-19 23:20:52 +0300

    Tasks 1, 2, 3

---
+++ Task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string inputString = Console.ReadLine();
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            foreach (char c in inputString)
+            {
+                if (dict.ContainsKey(c))
+                {
+                    dict[c] += 1;
+                }
+                else
+                {
+                    dict.Add(c, 1);
+                }
+                Console.Write(dict[c]);
+            }
+        }
+    }
+}
