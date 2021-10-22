d8af94b956d096a802188f911d0d2c6e3ca0cd91
karpenkoYegor <77341024+karpenkoYegor@users.noreply.github.com>
2021-10-22 12:01:31 +0300

    Добавил проверку

--- Task_5/Program.cs
+++ Task_5/Program.cs
@@ -7,38 +7,7 @@ namespace Task_5
         static void Main(string[] args)
         {
             int N = Convert.ToInt32(Console.ReadLine());
-            int[,] array = new int[N, N];
-            int x = 0;
-            int y = 0;
-            int sx = 1;
-            int sy = 0;
-            int steps = N-1;
-            int currentStep = steps;
-            int count = 0;
-
-            for (int i = 1; i <= N*N; i++)
-            {
-
-                currentStep--;
-                array[y, x] = i;
-                x += sx;
-                y += sy;
-
-                if (currentStep == 0)
-                {
-                    int a = sx;
-                    sx = -sy;
-                    sy = a;
-                    count++;
-                    if(count == 3)
-                    {
-                        count = 1;
-                        steps--;
-                    }
-                    currentStep = steps;
-                }
-            }
-
+            int[,] array = spiralMatrix(N);
             for (int a = 0; a < N; a++)
             {
                 for (int j = 0; j < N; j++)
@@ -47,7 +16,49 @@ namespace Task_5
                 }
                 Console.WriteLine();
             }
+        }
 
+        static int[,] spiralMatrix(int N)
+        {
+            if (N > 0)
+            {
+                int[,] array = new int[N, N];
+                int x = 0;
+                int y = 0;
+                int sx = 1;
+                int sy = 0;
+                int steps = N - 1;
+                int currentStep = steps;
+                int count = 0;
+
+                for (int i = 1; i <= N * N; i++)
+                {
+
+                    currentStep--;
+                    array[y, x] = i;
+                    x += sx;
+                    y += sy;
+
+                    if (currentStep == 0)
+                    {
+                        int a = sx;
+                        sx = -sy;
+                        sy = a;
+                        count++;
+                        if (count == 3)
+                        {
+                            count = 1;
+                            steps--;
+                        }
+                        currentStep = steps;
+                    }
+                }
+                return array;
+            }
+            else
+            {
+                return new int[0, 0];
+            }
         }
     }
 }
